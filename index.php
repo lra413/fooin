a3b9cd5fb47e75ed7eb6aed11053a2aa03162526
lra413 <rajeshwar6517@gmail.com>
2023-07-06 18:48:10 +0530

    commit

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    if (isset($_SESSION['style_prefernece']))
+    {
+        echo'<link rel=stylesheet href="normal_style.css"';
+    }
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
-
+/*
+the below is where we cheak if the website backend is up or down
+*/
 try{
-require
-}
-
-
-
-
-
-
-
-
-
-
+    require("blocks/navigation_bar.php");
+    }
+catch(Exception $e)
+    {
+        echo "the server is down";
+    }
 ?>
+    
+</body>
+</html>
